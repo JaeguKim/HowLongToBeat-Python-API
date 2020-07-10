028303b2c178e4addeff5c51cdce5f3acf6f3d78
김재구 <rlaworn1993@gmail.com>
2020-07-10 11:59:41 +0900

    modifed .travis.yml

---
+++ myenv/lib/python3.6/site-packages/hltbapi/HowLongToBeatEntry.py
@@ -0,0 +1,12 @@
+class HowLongToBeatEntry:
+    def __init__(self,detailId,gameName,description,playableOn,imageUrl,timeLabels,gameplayMain,gameplayMainExtra,gameplayCompletionist):
+        self.detailId = detailId
+        self.gameName = gameName
+        self.description = description
+        self.playableOn = playableOn
+        self.imageUrl = imageUrl
+        self.timeLabels = timeLabels
+        self.gameplayMain = gameplayMain
+        self.gameplayMainExtra = gameplayMainExtra
+        self.gameplayCompletionist = gameplayCompletionist
+        
